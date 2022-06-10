e3197f2bf737467922e16d90a5337f0732a49017
unknown <macrowell@yahoo.com>
2022-06-10 12:55:45 -0400

    set up get endpoints for projects router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,4 +2,25 @@
 const express = require('express');
 const router = express.Router();
 
+const Projects = require('./projects-model');
+
+router.get('/', (req, res) => {
+    Projects.get()
+    .then(projectsList => res.status(200).json(projectsList))
+    .catch(err => res.status(500).json({ message: "There was a problem retrieving the data" }))
+});
+
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id)
+    .then(project => {
+        if (!project) {res.status(404).json({ message: `there are no projects with id: ${req.params.id}` })}
+        if (project) {res.status(200).json(project)}
+    })
+    .catch(err => res.status(500).json(err.message))
+});
+
+router.delete('/:id', (req, res) => {
+    res.json({ message: `DELETE endpoint in projects router is receiving requests for id: ${req.params.id}` })
+});
+
 module.exports = router;
