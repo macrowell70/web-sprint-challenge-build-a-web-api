b49751b17500d873704922a12d36d80b05251758
unknown <macrowell@yahoo.com>
2022-06-11 12:09:35 -0400

    implemented middleware on actions router

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,54 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model');
+
+const validateId = (req, res, next) => {
+    Actions.get(req.params.id)
+    .then(action => {
+        if (!action) {res.status(404).json({ message: `there are no projects with id: ${req.params.id}` })}
+        if (action) { req.action = action }
+        next();
+    })
+    .catch(err => res.status(500).json(err.message))
+}
+
+const validatePost = (req, res, next) => {
+    const { notes, description, project_id } = req.body
+    Actions.insert(req.body)
+    .then(actionPost => {
+        req.actionPost = actionPost
+        next()
+    })
+    .catch(err => {
+        if (!notes || !description || !project_id) {
+            res.status(400).json({ message: "please provide name and description" })
+            return;
+        }
+        res.status(500).json(err.message)
+    })
+}
+
+const validateUpdate = (req, res, next) => {
+    const { description, completed } = req.body
+    Actions.update(req.params.id, req.body)
+    .then(actionPost => {
+        if (!description || completed == null) {
+            res.status(400).json({ message: "please provide completed status" })
+            return;
+        }
+        req.actionPost = actionPost
+        next()
+    })
+    .catch(err => {
+        if (!description || completed == null) {
+                res.status(400).json({ message: "please provide completed status" })
+                return;
+        }
+        res.status(500).json(err.message)
+    })   
+}
+
+module.exports = {
+    validateId,
+    validateUpdate,
+    validatePost,
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,53 +4,28 @@ const router = express.Router();
 
 const Actions = require('./actions-model');
 
-router.get('/', (req, res) => {
-    Actions.get()
-    .then(actionsList => res.json(actionsList))
-    .catch(err => res.json(err.message))
+const { validateId, validatePost, validateUpdate } = require('./actions-middlware');
+
+router.get('/', validateId, (req, res) => {
+    res.status(200).json(req.action)
 });
 
-router.get('/:id', (req, res) => {
-    Actions.get(req.params.id)
-    .then(action => {
-        if (!action) {res.status(404).json({ message: `there are no Actions with id: ${req.params.id}` })}
-        if (action) {res.status(200).json(action)}
-    })
-    .catch(err => res.status(500).json(err.message))
+router.get('/:id', validateId, (req, res) => {
+    res.status(200).json(req.action)
 });
 
-router.post('/', (req, res) => {
-    Actions.insert(req.body)
-    .then(action => {res.status(201).json(action)})
-    .catch(err => {
-        if (!req.body.name || !req.body.description) {
-            res.status(400).json({ message: "please provide name and description" })
-            return;
-        }
-        res.status(500).json(err.message)
-    })
+router.post('/', validatePost, (req, res) => {
+    res.status(201).json(req.actionPost)
 });
 
-router.put('/:id', (req, res) => {
-    const { name, description, completed } = req.body
-    Actions.update(req.params.id, req.body)
-    .then(action => {res.status(201).json(action)})
-    .catch(err => {
-        if (!name || !description || completed == null) {
-                res.status(400).json({ message: "please provide name, description, and completed" })
-                return;
-        }
-        res.status(500).json(err.message)
-    })
+router.put('/:id', validateId, validateUpdate, (req, res) => {
+    res.status(200).json(req.actionPost)
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateId, (req, res) => {
     Actions.remove(req.params.id)
-    .then(action => {
-        if (!action) {res.status(404).json({ message: `there are no Actions with id: ${req.params.id}` })}
-        if (action) {res.end()}
-    })
+    .then(project => {res.end()})
     .catch(err => res.status(500).json(err.message))
 });
 
